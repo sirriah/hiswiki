b2bf4d4ef38e0973854965eb4de52725be6710e7
Tereza Fatková <65444975+sirriah@users.noreply.github.com>
2022-02-24 15:46:27 +0100

    Add basic setup (#1)

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  return (
-    <h1 className='text-3xl font-bold underline'>hello world</h1>
-  )
+  return <h1 className="text-3xl font-bold underline">hello world</h1>;
 }
