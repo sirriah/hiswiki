2e088518baa57ea635a03ee2597ec213a25b31fc
Lukas Cizek <globus303@gmail.com>
2022-02-24 21:19:42 +0100

    Fix husky hook, update eslint config

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
-import Head from 'next/head';
-import Image from 'next/image';
+const Home = () => (
+  <h1 className="text-3xl font-bold underline">hello world</h1>
+);
 
-import styles from '../styles/Home.module.css';
-
-export default function Home() {
-  return <h1 className="text-3xl font-bold underline">hello world</h1>;
-}
+export default Home;
