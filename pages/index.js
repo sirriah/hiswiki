1341e4ae7125b8570dcce7f48352b56bedfa0926
Tereza Fatková <65444975+sirriah@users.noreply.github.com>
2022-03-04 22:44:14 +0100

    Add basic layout (#3)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,17 @@
+import { Layout } from '../components/Layout';
+
 const Home = () => (
-  <h1 className="text-3xl font-bold underline">hello world</h1>
+  <Layout>
+    <div className="font-yrsa">This is a test</div>
+    <p className="mx-36">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. In condimentum,
+      arcu vel tempus finibus, nulla metus congue lacus, ac vehicula ex nulla
+      non erat. Phasellus ultrices sapien sit amet eros ullamcorper lacinia. Sed
+      at sem facilisis ipsum fermentum pulvinar. Nam volutpat fringilla mi,
+      vitae mollis mi lobortis quis. Vivamus finibus ex est, interdum bibendum
+      dolor consequat in. Duis ut porta nunc. Vivamus tempor accumsan finibus.
+    </p>
+  </Layout>
 );
 
 export default Home;
