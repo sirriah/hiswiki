336f6ba42656aa3b9661dd9f42ccc00c382ad6ab
Tereza Fatková <65444975+sirriah@users.noreply.github.com>
2022-04-01 16:02:10 +0200

    Add footer design (#6)
    * Add footer design
    
    * Margin bottom on links in footer
    
    * Minification of SVG
    
    * Delete the original svg file
    
    * Update components/Navigation/Navigation.js
    
    Co-authored-by: Lukas Cizek <globus303@gmail.com>
    
    * Classnames simplyfication
    
    Co-authored-by: Lukas Cizek <globus303@gmail.com>

--- components/Header.js
+++ components/Header.js
@@ -24,7 +24,7 @@ export const Header = () => {
           isHeaderTransparent ? 'lg:bg-dark-300' : 'lg:bg-transparent'
         }`}
       >
-        <div className="max-w-screen-xl mx-auto py-2 px-4 md:p-5 flex relative">
+        <div className="max-w-screen-xl mx-auto py-2 px-4 md:p-3 flex relative lg:items-center">
           <Link href="/home">
             <a className="font-yrsa font-bold uppercase text-4xl leading-none pr-8 sm:text-black lg:text-white hover:text-opacity-80 transition duration-200">
               Hiswiki
--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -12,35 +12,8 @@ export const Navigation = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const renderMobileMenu = () => (
-    <nav className="bg-accent-200 absolute top-[52px] md:top-[76px] lg:invisible right-0 p-3 z-50">
-      <ul className="flex flex-col">
-        <li className="divide-y-2 divide-black">
-          <Link href="/new-article">
-            <a className="divide-y-2 divide-black p-3">Nový článek</a>
-          </Link>
-        </li>
-        <li className="divide-y-2 divide-black">
-          <Link href="/profile">
-            <a className="divide-y-2 divide-black p-3">Profil</a>
-          </Link>
-        </li>
-        <li className="divide-y-2 divide-black">
-          <Link href="/logout">
-            <a className="divide-y-2 divide-black p-3">Odhlasit se</a>
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
-
-  const hamburgerTopClass = classNames({ 'rotate-45': isMenuOpen });
-  const hamburgerMiddleClass = classNames({ 'opacity-0': isMenuOpen });
-  const hamburgerBottomClass = classNames({ '-rotate-45': isMenuOpen });
-
   return (
     <>
-      {isMenuOpen && renderMobileMenu()}
       <div className=" hamburger flex-1 lg:hidden relative">
         <button
           className="absolute right-0 block w-11 h-10"
@@ -48,31 +21,46 @@ export const Navigation = () => {
         >
           <span className="sr-only">Menu</span>
           <span
-            className={` ${hamburgerTopClass} transition ease-in-out duration-300 origin-top-left w-[26px] h-[2px] top-[6px] right-2 bg-slate-500 block absolute`}
+            className={classNames(
+              { 'rotate-45': isMenuOpen },
+              'transition ease-in-out duration-300 origin-top-left w-[26px] h-[2px] top-[6px] right-2 bg-slate-500 block absolute',
+            )}
           />
           <span
-            className={`${hamburgerMiddleClass} transition ease-in-out duration-300 w-[26px] h-[2px] top-[15px] right-2 bg-slate-500 block absolute`}
+            className={classNames(
+              { 'opacity-0': isMenuOpen },
+              'transition ease-in-out duration-300 w-[26px] h-[2px] top-[15px] right-2 bg-slate-500 block absolute',
+            )}
           />
           <span
-            className={`${hamburgerBottomClass} transition ease-in-out duration-300 origin-bottom-left w-[26px] h-[2px] top-[24px] right-2 bg-slate-500 block absolute`}
+            className={classNames(
+              { '-rotate-45': isMenuOpen },
+              'transition ease-in-out duration-300 origin-bottom-left w-[26px] h-[2px] top-[24px] right-2 bg-slate-500 block absolute',
+            )}
           />
         </button>
       </div>
-      <nav className="flex-1 hidden lg:flex" aria-label="Hlavní menu">
-        <ul className="flex-1 flex justify-end items-center divide-x-2 divide-accent-500">
-          <li>
+      <nav
+        className={classNames(
+          { hidden: !isMenuOpen },
+          'bg-accent-200 absolute top-[52px] md:top-[76px] right-0 z-50 lg:bg-transparent lg:relative lg:flex-1 lg:flex lg:top-0',
+        )}
+        aria-label="Hlavní menu"
+      >
+        <ul className="flex flex-col divide-y-2 justify-end items-center divide-accent-500 lg:flex-row lg:divide-y-0 lg:divide-x-2 lg:flex-1">
+          <li className="py-4">
             <Link href="/new-article">
-              <a className="px-8 hover:underline hover:underline-offset-8 hover:decoration-accent-500 hover:decoration-2">
+              <a className="px-8 lg:hover:underline hover:underline-offset-8 hover:decoration-accent-500 hover:decoration-2">
                 Nový článek
               </a>
             </Link>
           </li>
-          <li className="flex items-center">
+          <li className="py-2 flex items-center lg:py-0">
             <Link href="/user-profile">
-              <a className="flex items-center px-8 hover:underline hover:underline-offset-8 hover:decoration-accent-500 hover:decoration-2">
+              <a className="flex items-center px-8 lg:hover:underline hover:underline-offset-8 hover:decoration-accent-500 hover:decoration-2">
                 <div className="mr-4">
                   <Image
-                    className="w-9 h-9 rounded-full pr-8 block"
+                    className="w-9 rounded-full pr-8 block"
                     src={profilePic}
                     alt=""
                     width="36"
@@ -83,9 +71,9 @@ export const Navigation = () => {
               </a>
             </Link>
           </li>
-          <li className="flex items-center h-[42px]">
+          <li className="py-4 w-full flex items-center lg:w-auto lg:max-h-[42px]">
             <Link href="/log-out">
-              <a className="px-8  hover:underline hover:underline-offset-8 hover:decoration-accent-500 hover:decoration-2">
+              <a className="px-8 lg:hover:underline hover:underline-offset-8 hover:decoration-accent-500 hover:decoration-2">
                 Odhlásit se
               </a>
             </Link>
