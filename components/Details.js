ea2650a6c48d1863cd42db4330e659bd585e0980
sirriah <tereza.dvorakova.3@seznam.cz>
2022-05-16 15:34:57 +0200

    Add values to Details

--- components/Details.js
+++ components/Details.js
@@ -14,22 +14,35 @@ export const Details = () => {
     setDetailFields(data);
   };
 
+  const handleFormChange = (index, event) => {
+    const data = [...detailFields];
+    data[index][event.target.name] = event.target.value;
+    setDetailFields(data);
+  };
+
+  // eslint-disable-next-line no-console
+  console.log(detailFields);
+
   return (
     <ol className="list-inside list-decimal">
       {detailFields.map((fields, index) => (
         <li key={index} className="mb-4 border-b-2 bg-accent-50 p-4">
           <FormInput
             label="Název"
+            name="fieldName"
             labelClassName="mt-6 mb-1 block text-sm text-stone-700"
             className="mb-4 block w-full border-b-2 border-stone-300 bg-white p-2"
-            defaultValue={fields.fieldName}
+            value={fields.fieldName}
+            onChange={(event) => handleFormChange(index, event)}
             id={`fieldName[${index}]`}
           />
           <FormInput
             label="Obsah"
+            name="fieldContent"
             labelClassName="mt-6 mb-1 block text-sm text-stone-700"
             className="mb-4 block w-full border-b-2 border-stone-300 bg-white p-2"
-            defaultValue={fields.fieldContent}
+            value={fields.fieldContent}
+            onChange={(event) => handleFormChange(index, event)}
             id={`fieldContent[${index}]`}
           />
 
